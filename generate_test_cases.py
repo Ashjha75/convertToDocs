15ed4d12b81dcbcd3046f2dfa67234652c96c99f
Ashish jha <ajha5645@gmail.com>
2025-12-23 12:29:48 +0530

    feat: Update input JSON file for test case generation

--- generate_test_cases.py
+++ generate_test_cases.py
@@ -10,7 +10,7 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Inches, Pt, RGBColor
 
 # ---------- CONFIG ----------
-INPUT_JSON = "partnermobileword.json"  # path to your JSON file
+INPUT_JSON = "rms-deposit.json"  # path to your JSON file
 OUTPUT_DOCX = "Mpos-Biller-Portal-Test_Cases.docx"
 OUTPUT_ZIP = "Mpos-Biller-Portal-Test_Cases.zip"
 TITLE_TEXT = "Mpos-Biller Portal Test Cases"
