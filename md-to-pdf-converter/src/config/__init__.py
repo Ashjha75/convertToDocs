3424bf9b987eb93362825f2f5c2984b90c6c4aeb
Ashish jha <ajha5645@gmail.com>
2025-12-25 16:42:17 +0530

    Remove deprecated files: LICENSE, README.md, install.bat, configuration settings, main entry point, and sample markdown document

--- md-to-pdf-converter/src/config/__init__.py
+++
@@ -1,4 +0,0 @@
-# Config package initialization
-from .settings import PDF_CONFIG, CSS_TEMPLATE, FONT_CONFIG
-
-__all__ = ['PDF_CONFIG', 'CSS_TEMPLATE', 'FONT_CONFIG']
--- md-to-pdf-converter/src/converters/__init__.py
+++
@@ -1,4 +0,0 @@
-# Converters package initialization
-from .md_to_pdf import MdToPdfConverter
-
-__all__ = ['MdToPdfConverter']
