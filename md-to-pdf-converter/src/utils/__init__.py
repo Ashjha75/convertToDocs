d2bb294c28a5125ba0f9af5b6a0df8d23e7be74e
Ashish jha <ajha5645@gmail.com>
2025-12-25 16:42:09 +0530

    Remove unnecessary files: .gitignore, GITHUB_INFO.md, and utility scripts for Markdown parsing and PDF styling. Update ppt.ipynb to clear output and streamline installation commands.

--- md-to-pdf-converter/src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# Utils package initialization
-from .markdown_parser import parse_markdown_with_code
-from .pdf_styler import apply_custom_styles
-
-__all__ = ['parse_markdown_with_code', 'apply_custom_styles']
